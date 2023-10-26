91b1b2205f1a56e6f358a7f42b7be69633f112f4
Duvan_vivas <duvan.vivas@zibordesarrolladores.com>
2023-10-26 11:01:26 -0500

    add push project new for test a know this project

---
+++ react-js+boostrap/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Mavbar from './components/Navbar.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+
+  return (
+    <>
+      <div>
+          <Mavbar/>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
